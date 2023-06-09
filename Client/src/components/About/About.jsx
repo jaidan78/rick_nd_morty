40d0a1800bd7597bfbd57a09e0ab3bf25b8de344
jaidan78 <jaime.tablet25@gmail.com>
2023-06-09 02:25:39 -0300

    first commit

---
+++ Client/src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { styles } from "./About.module.css";
+
+const About = () => {
+  return (
+    <div>
+      <h1>{"©2023 Jaime Baldomá"}</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ Server/src/utils/users.js
@@ -0,0 +1,4 @@
+module.exports = [
+  { email: "jaime.tablet25@gmail.com", password: "Mito1234" },
+  { email: "", password: "" },
+];
