f58d127bf29c0ab55f372c693ccca5d1c0edd042
jaidan78 <jaime.tablet25@gmail.com>
2023-06-22 22:35:26 -0300

    22.06.2023 22:35

--- Server/src/index.js
+++ Server/src/index.js
@@ -38,7 +38,11 @@
 
 const server = require("./app");
 
-const PORT = 3001;
-server.listen(PORT, () => {
-  console.log("Server raised in port: " + PORT);
-});
+const { conn } = require('./DB_connection');
+
+conn.sync({ force:true }).then(()=>{
+  server.listen('3001', () => {
+    console.log("Server raised in port: ",3001);
+  });
+})
+
